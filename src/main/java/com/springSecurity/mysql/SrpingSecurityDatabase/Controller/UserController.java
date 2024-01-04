0de813c5632059b2278f85107a8c9bf36277a6fd
Ritesh18117 <rgriteshbyahut18117rg@gmail.com>
2024-01-04 10:44:04 +0530

    first commit

---
+++ src/main/java/com/springSecurity/mysql/SrpingSecurityDatabase/Controller/UserController.java
@@ -0,0 +1,66 @@
+package com.springSecurity.mysql.SrpingSecurityDatabase.Controller;
+
+import com.springSecurity.mysql.SrpingSecurityDatabase.entities.AuthRequest;
+import com.springSecurity.mysql.SrpingSecurityDatabase.entities.UserInfo;
+import com.springSecurity.mysql.SrpingSecurityDatabase.service.JwtService;
+import com.springSecurity.mysql.SrpingSecurityDatabase.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    @Autowired
+    private UserInfoService service;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @GetMapping("/getAllUser")
+    public ResponseEntity<List<UserInfo>> getAllUser(){
+        return service.getAllUser();
+    }
+
+    @PostMapping("/addNewUser")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/user/userProfile")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String userProfile() {
+        return "Welcome to User Profile";
+    }
+
+    @GetMapping("/admin/adminProfile")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String adminProfile() {
+        return "Welcome to Admin Profile";
+    }
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+    }
+}
---
+++ src/main/java/com/springSecurity/mysql/SrpingSecurityDatabase/SrpingSecurityDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.springSecurity.mysql.SrpingSecurityDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingSecurityDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingSecurityDatabaseApplication.class, args);
+	}
+
+}
